3da9cb1a5042e76f20d8fb38fec16f233253e987
AnnieRod <euphie.13@gmail.com>
2022-09-01 20:47:10 -0500

    agregue index a los elementos de la tabla

--- tabla_html/server.py
+++ tabla_html/server.py
@@ -4,15 +4,12 @@ app = Flask(__name__)
 
 @app.route ('/')
 def render_users():
-    count = 1
     users = [
         {'first_name' : 'Michael', 'last_name' : 'Choi'},
         {'first_name' : 'John', 'last_name' : 'Supsupin'},
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
         {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]   
-    count+=1
     return render_template("index.html", users = users)
-
 if __name__=="__main__":   
     app.run(debug=True)    
